b03239b30673a002d1f238f3aa15ef95a57a8da9
user <brewbaker@yandex.com>
2017-08-20 06:26:02 +0300

    Create _create_test_table.php

---
+++ _create_test_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('разделы', function (Blueprint $table) {
+            $table->increments('id'); // id INT AUTO_INCREMENT
+            $table->string('title',100); // Varchar 100
+            $table->text('text'); //text
+            $table->float('price'); //double(100,2)
+            $table->string('metadata',100); // Varchar 100
+            $table->string('img',255);//Varchar 100
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+         Schema::drop('разделы'); //DROP TABLE `разделы`;
+     }
+ }
