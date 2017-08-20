02a67a0b63c06cbd81cc4b0d4a2338b05519f32c
user <brewbaker@yandex.com>
2017-08-20 07:52:36 +0300

    Update change_table.php

--- tmp/change_table.php
+++ tmp/change_table.php
@@ -22,7 +22,7 @@ class ChangeTestTable extends Migration
      * Reverse the migrations.
      *
      * @return void
-     *//*
+     */
     public function down()
     {
         Schema::table('разделы', function (Blueprint $table) {
