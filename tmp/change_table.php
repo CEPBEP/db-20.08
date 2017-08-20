9da32d3ef8ea911d219d48c05113ce660468f2cf
user <brewbaker@yandex.com>
2017-08-20 07:52:18 +0300

    Create change_table.php

---
+++ tmp/change_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('разделы', function (Blueprint $table) {
+           $table ->string('что-то новенькое',100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     *//*
+    public function down()
+    {
+        Schema::table('разделы', function (Blueprint $table) {
+           $table->dropColumn('что-то новенькое'); //
+        });
+    }
+}?>
